03a9472d749d0c7a911a499f273d45be36c93816
Husnulzaki Haryadi <husnulzaki.haryadi@gmail.com>
2023-04-01 15:04:10 +0700

    Implement ONNXModel

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import numpy as np
+import onnxruntime
 from PIL import Image
 from torch import Tensor
 from torchvision import transforms
@@ -16,7 +18,31 @@ def preprocess_numpy(img: Image) -> Tensor:
     return img.unsqueeze(0)
 
 
+class ONNXModel:
+    def __init__(self, onnx_file: str):
+        self._session = onnxruntime.InferenceSession(onnx_file)
+
+    def _to_numpy(self, tensor: Tensor) -> np.ndarray:
+        return (
+            tensor.detach().cpu().numpy()
+            if tensor.requires_grad
+            else tensor.cpu().numpy()
+        )
+
+    def predict(self, image: Image) -> int:
+        inputs = {self._session.get_inputs()[0].name: self._to_numpy(image)}
+        outputs = self._session.run(None, inputs)
+        # get the highest probability in `outputs` as the predicted classification
+        final_out = np.array(outputs).argmax()
+        return final_out
+
+
 if __name__ == "__main__":
+    # Load and prepare image
     img = Image.open("./resources/n01667114_mud_turtle.jpeg")
-    img = preprocess_numpy(img).unsqueeze(0)
-    print(img)
+    img = preprocess_numpy(img)
+
+    # Load and run the model
+    onnx_model = ONNXModel("./onnx_model.onnx")
+    prediction = onnx_model.predict(img)
+    print(prediction)
